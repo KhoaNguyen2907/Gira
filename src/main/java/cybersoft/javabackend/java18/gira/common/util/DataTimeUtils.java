0234ae485ace5edc0353c7c6975076cfb13c0f30
KhoaNguyen2907 <99467514+KhoaNguyen2907@users.noreply.github.com>
2022-09-18 15:51:07 +0700

    feat: base entity with jpa auditor, cjb-3

---
+++ src/main/java/cybersoft/javabackend/java18/gira/common/util/DataTimeUtils.java
@@ -0,0 +1,9 @@
+package cybersoft.javabackend.java18.gira.common.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DataTimeUtils {
+    public static final String DATE_TIME_FORMAT = "HH:mm:ss dd-MM-yyyy";
+
+}
